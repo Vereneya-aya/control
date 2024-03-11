dc6995c220f13fd4dab6de835f4b5367333497cd
VL <veranikalis11@gmail.com>
2024-03-11 12:46:50 +0100

    program added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк 
+// формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести 
+// с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] array = { "13", "no", "3", "hello", "war" };
+int n = new Random().Next(0, 4);
+string[] new_array = new string[n];
+for (int i = 0; i < n; i++)
+{
+    new_array[i] = array[new Random().Next(0, array.Length)];
+}
+Console.WriteLine($"Новый массив: [ {string.Join("; ", new_array)} ]");
